c93cfdf719803f0b73d19f1768a83568f089236d
Juan Manuel Losada <juanmalosada01@gmail.com>
2026-03-10 12:58:43 -0300

    Updates

--- assets/check-Di-EwC94.js
+++
@@ -1,6 +0,0 @@
-import{am as c}from"./index-CzzS8G7y.js";/**
- * @license lucide-react v0.563.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
